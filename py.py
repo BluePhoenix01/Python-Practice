9ef377b424411b4b28428d69785c745147cbf1c5
= <=>
2024-03-01 17:32:41 -0500

    Generators and Debugging

--- py.py
+++ py.py
@@ -22,4 +22,27 @@
 # a, _, __ = variable_FS.partition("bye")
 # print(a, _, __)
 
+#Generators
+# def generate_ID(start):
+#     count = start
+#     while True:
+#         yield count
+#         count += 1
+    
+# id_generator = generate_ID(6782)
 
+# print(next(id_generator))
+# print(next(id_generator))
+
+# Python Debugger
+# def fact(num):
+#     fact = 1
+#     for i in range(1, num+1):
+#         breakpoint()
+#         fact *= i
+#     return fact
+
+# print(fact(10))
+
+assert spam < 10
+assert spam.lower() == bacon
