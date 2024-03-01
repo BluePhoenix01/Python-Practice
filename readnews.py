2859515040679eb7f4966fb9a78b708e8bef1206
= <=>
2024-03-01 04:46:39 -0500

    Web Scraper

---
+++ readnews.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = 'https://www.bbc.com'
+
+URL = BASE_URL+'/sport'
+res = requests.get(URL)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.find_all(class_='ssrcss-vdnb7q-PromoLink exn3ah91')
+
+for link in links:
+    route = link.get('href')
+    if not route.startswith('/sport/football'):
+        continue
+    subres = requests.get(BASE_URL+route)
+    soup2 = BeautifulSoup(subres.text, 'html.parser')
+    title = soup2.find(class_='gel-trafalgar-bold qa-story-headline gs-u-mv+')
+    print(title.text)
+
+
+
+
+
