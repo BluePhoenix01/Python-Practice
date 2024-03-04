4a6ec94d9bf0f25c925cdf5a71916398ba03cbbc
= <=>
2024-03-03 19:26:16 -0500

    Added Comments and removed debug prints

--- currencyconverter.py
+++ currencyconverter.py
@@ -4,6 +4,7 @@ from tkinter import Tk, ttk
 import requests
 from dotenv import load_dotenv
 
+# load environment variables from .env file
 load_dotenv()
 
 root = Tk()
@@ -13,15 +14,15 @@ frm = ttk.Frame(root, padding=10)
 frm.grid()
 
 
-def validate_amount(text, current_text, action):
+def validate_amount(new_text, current_val, action):
+    """Makes sure amount entered is a valid float"""
+    # if action == 0 aka delete then skip the validation
     if int(action) == 0:
         return True
-    if not text:
+    if not new_text:
         return True
     try:
-        print(current_text)
-        print(text)
-        float(current_text + text)
+        float(current_val + new_text)
         return True
     except ValueError:
         return False
@@ -31,6 +32,7 @@ vamt = root.register(validate_amount)
 
 
 def convert():
+    """Does the conversiona and inserts result in a label below"""
     from_currency = from_curr.get()
     to_currency = to_curr.get()
     amount = amt.get()
@@ -59,22 +61,32 @@ def convert():
 
 ttk.Label(frm, text="Currency Converter").grid(column=0, columnspan=5, row=0)
 
+
 ttk.Label(frm, text="From").grid(column=0, row=1)
 from_curr = ttk.Entry(frm, width=10)
 from_curr.grid(row=1, column=1, pady=5)
+
 
 ttk.Label(frm, text="To").grid(column=2, row=1)
 to_curr = ttk.Entry(frm, width=10)
 to_curr.grid(row=1, column=3, pady=5)
 
+
 ttk.Label(frm, text="Amount").grid(column=4, row=1)
+
+# Validate command args
+# %S = the text string being inserted or deleted, if any
+# %s = value of entry prior to editing
+# %d = Type of action (1=insert, 0=delete, -1 for others)
 amt = ttk.Entry(frm, width=10, validate="key", validatecommand=(vamt, "%S", "%s", "%d"))
 amt.grid(row=1, column=5, pady=5)
 
 
 ttk.Button(frm, text="Convert", command=convert).grid(column=6, row=1)
 
+
 result_label = ttk.Label(frm, text="")
 result_label.grid(row=2, column=0, columnspan=6)
+
 
 root.mainloop()
